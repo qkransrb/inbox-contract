afc7e798796e80d197686a7e73b86a0978202394
qkransrb <qkransrb90@gmail.com>
2021-11-16 00:03:03 +0900

    inbox contract solidity ^0.4.17

---
+++ deploy.js
@@ -0,0 +1,33 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  // mnemonic,
+  "mnemonic",
+  // infura rinkeby address
+  "rinkeby address"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("> Attempting to deploy from account: ", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["initial message"],
+    })
+    .send({
+      from: accounts[0],
+      gas: "1000000",
+    });
+
+  console.log("Contract deployed to ", result.options.address);
+
+  provider.engine.stop();
+};
+
+deploy();
